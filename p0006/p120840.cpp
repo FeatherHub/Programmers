5225ba19ba75b1e46380cd7b398505dbc2bb78d1
Featherhub <featherhub@github.com>
2025-12-13 18:10:56 +0900

    update clangd config

--- p0006/p120840.cpp
+++ p0006/p120840.cpp
@@ -1,9 +1,8 @@
-
 #include <algorithm>
-#include <functional>
 using namespace std;
 
 int pick_m_from_n(int n, int m) {
+
   using Integer = unsigned long long;
 
   // formula: n! / ((n-m)! * m!) = C(n, m)
@@ -20,9 +19,7 @@ int pick_m_from_n(int n, int m) {
 
   const Integer bigger = max(n - m, m);
   const Integer smaller = min(n - m, m);
-
-  greater<int>();
-
+  
   Integer prod = 1;
   for (Integer i = 1; i <= smaller; ++i) {
     prod *= bigger + i;
@@ -35,21 +32,21 @@ int pick_m_from_n(int n, int m) {
 int solution(int balls, int share) { return pick_m_from_n(balls, share); }
 
 int main() {
-  (void) solution(3, 2);
-  (void) solution(5, 3);
-  (void) solution(1, 1);
-  (void) solution(5, 3);
-  (void) solution(5, 1);
-  (void) solution(6, 6);
-  (void) solution(10, 9);
-  (void) solution(30, 3);
-  (void) solution(30, 3);
-  (void) solution(30, 29);
-  (void) solution(30, 25);
-  (void) solution(25, 20);
-  (void) solution(30, 2);
-  (void) solution(30, 17);
-  (void) solution(30, 15);
+  solution(3, 2);
+  solution(5, 3);
+  solution(1, 1);
+  solution(5, 3);
+  solution(5, 1);
+  solution(6, 6);
+  solution(10, 9);
+  solution(30, 3);
+  solution(30, 3);
+  solution(30, 29);
+  solution(30, 25);
+  solution(25, 20);
+  solution(30, 2);
+  solution(30, 17);
+  solution(30, 15);
 
   return 0;
 }
